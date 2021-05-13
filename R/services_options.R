8f1f77129837c96ae387575ee15e8f6a33a0da4f
MalditoBarbudo <victorgrandagarcia@gmail.com>
2021-05-13 17:05:01 +0200

    updated service_options help and examples

--- R/services_options.R
+++ R/services_options.R
@@ -3,14 +3,13 @@
 #' Set the options for accessing the different spanish meteorological services
 #'
 #' @param resolution Character indicating the temporal resolution for the data. Services allows different
-#'   temporal resolutions that can be present or not in each of them (hourly, daily, monthly, instant).
+#'   temporal resolutions that can be present or not in each of them (current_day, instant, daily, monthly).
 #' @param start_date Date class object with the start date from which start collecting data. Ignored if
 #'   resolution is one of \code{current_day} or \code{instant}.
-#' @param end_date Date class object with the end date from which stop collecting data. If NULL (default),
-#'   only the date in \code{start_date} is returned. Ignored if resolution is one of \code{current_day} or
-#'   \code{instant}.
+#' @param end_date Date class object with the end date from which stop collecting data. By default, same
+#'   date as \code{start_date}. Ignored if resolution is one of \code{current_day} or \code{instant}.
 #' @param stations Character vector with the stations codes from which extract data from. If NULL (default)
-#'   all available stations are returned.
+#'   all available stations are returned. See Stations section for more details.
 #' @param api_key Character with the API key. NULL by default as not all services require keys. See API Keys
 #'   section for more details.
 #'
@@ -20,7 +19,6 @@
 #' \itemize{
 #'   \item{\code{current_day} returns the last 24h of measures.}
 #'   \item{\code{instant} returns the last measure available.}
-#'   \item{\code{hourly} returns any past date/s in hourly format.}
 #'   \item{\code{daily} returns any past date/s with daily aggregation.}
 #'   \item{\code{monthly} returns any past date/s with monthly aggregation.}
 #' }
@@ -35,7 +33,18 @@
 #' }
 #' It is not advisable to use the keys directly in any script shared or publicly available (github...), neither
 #' store them in plain text files.One option is using the keyring package
-#' (\link{https://github.com/r-lib/keyring}) for managing and accesing keys.
+#' (\link{https://github.com/r-lib/keyring}) for managing and accessing keys.
+#'
+#' @section Stations:
+#' Some services accept querying multiple stations at once, and other only allows one station per query:
+#' \itemize{
+#'   \item{AEMET: One or more stations can be provided in a character vector.}
+#'   \item{MeteoGalicia: One or more stations can be provided in a character vector.}
+#'   \item{Meteoclimatic: Only one station can be provided. Nevertheless, some codes can be used to retrieve
+#'   common group of stations: "ES" for all spanish stations, "ESCAT", "ESCYL", "ESAND"... for the different
+#'   autonomous communities.}
+#' }
+#'
 #'
 #' @name services_options
 #' @return A list with the service API options to make the query to obtain the data.
@@ -44,21 +53,27 @@ NULL
 
 #' Options for AEMET service
 #'
-#' Set the options for accessing the AEMET service
-#'
 #' @examples
 #' library(keyring)
 #' library(meteospain)
-#' library(lubridate)
+#'
+#' ## AEMET examples ---------------------------------------------------------
 #'
 #' # setting the key (a prompt will appear in console to supply the API key)
-#' keyring::key_set(service = 'aemet', user = 'me')
+#' # keyring::key_set(service = 'aemet')
 #'
-#' # create the options
-#' query_options <- aemet_options(
-#'   dates = as_date(as_date('1990-01-01'):as_date('1990-02-01')),
+#' # Options for the last 24h data
+#' current_opts <- aemet_options(
+#'   resolution = 'current_day',
+#'   api = keyring::key_get('aemet')
+#' )
+#'
+#' # Options for daily data for January, 1990
+#' daily_opts <- aemet_options(
 #'   resolution = 'daily',
-#'   api = keyring::key_get('aemet', 'me')
+#'   start_date = as.Date('1990-01-01'),
+#'   end_date = as.Date('1990-01-31'),
+#'   api = keyring::key_get('aemet')
 #' )
 #'
 #' @rdname services_options
@@ -67,7 +82,7 @@ NULL
 aemet_options <- function(
   resolution = c('current_day', 'daily'),
   start_date = Sys.Date(),
-  end_date = NULL,
+  end_date = start_date,
   stations = NULL,
   api_key
 ) {
@@ -75,7 +90,7 @@ aemet_options <- function(
   resolution <- rlang::arg_match(resolution)
   assertthat::assert_that(
     assertthat::is.date(start_date),
-    dplyr::if_else(rlang::is_null(end_date), TRUE, assertthat::is.date(end_date)),
+    assertthat::is.date(end_date),
     dplyr::if_else(rlang::is_null(stations), TRUE, rlang::is_character(stations)),
     rlang::is_character(api_key)
   )
@@ -84,7 +99,7 @@ aemet_options <- function(
   res <- list(
     resolution = resolution,
     start_date = start_date,
-    end_date = dplyr::if_else(rlang::is_null(end_date), start_date, end_date),
+    end_date = end_date,
     stations = stations,
     api_key = api_key
   )
@@ -94,21 +109,18 @@ aemet_options <- function(
 
 #' Options for SMC service
 #'
-#' Set the options for accessing the SMC service
-#'
 #' @examples
-#' library(keyring)
-#' library(meteospain)
-#' library(lubridate)
+#'
+#' ## SMC examples -----------------------------------------------------------
 #'
 #' # setting the key (a prompt will appear in console to supply the API key)
-#' keyring::key_set(service = 'smc', user = 'me')
+#' keyring::key_set(service = 'smc')
 #'
 #' # create the options
 #' query_options <- smc_options(
 #'   dates = as_date(as_date('1990-01-01'):as_date('2020-12-31')),
 #'   resolution = 'daily',
-#'   api = keyring::key_get('smc', 'me')
+#'   api = keyring::key_get('smc')
 #' )
 #'
 #' @rdname services_options
@@ -139,17 +151,13 @@ smc_options <- function(
 
 #' Options for Meteoclimatic service
 #'
-#' Set the options for accessing the Meteoclimatic service
-#'
 #' @examples
-#' library(meteospain)
-#' library(lubridate)
 #'
-#' query_options <- meteoclimatic_options()
-#' query_options
+#' ## Meteoclimatic examples -------------------------------------------------
+#'
+#' current_opts <- meteoclimatic_options()
 #' # same as before, but more verbose
-#' query_options <- meteoclimatic_options(stations = 'ES', resolution = 'present_day')
-#' query_options
+#' current_opts <- meteoclimatic_options(resolution = 'current_day', stations = 'ES')
 #'
 #' @rdname services_options
 #'
@@ -179,16 +187,30 @@ meteoclimatic_options <- function(
 
 #' Options for MeteoGalicia service
 #'
-#' Set the options for accessing the MeteoGalicia service
-#'
 #' @examples
-#' library(meteospain)
-#' library(lubridate)
 #'
-#' # create the options
-#' query_options <- meteogalicia_options(
-#'   dates = as_date(as_date('1990-01-01'):as_date('2020-12-31')),
-#'   resolution = 'daily'
+#' ## MeteoGalicia examples --------------------------------------------------
+#'
+#' # Options for the last measured data
+#' instant_opts <- meteogalicia_options(resolution = 'instant')
+#'
+#' # Options for the last 24h data
+#' current_opts <- meteogalicia_options(resolution = 'current_day')
+#' # same, with stations
+#' current_opts <- meteogalicia_options('current_day', stations = c('10045', '10046'))
+#'
+#' # Options for daily data for January, 2000
+#' daily_opts <- meteogalicia_options(
+#'   resolution = 'daily',
+#'   start_date = as.Date('2000-01-01'),
+#'   end_date = as.Date('2000-01-31')
+#' )
+#'
+#' # Options for monthly data for year 2000
+#' monthly_opts <- meteogalicia_options(
+#'   resolution = 'monthly',
+#'   start_date = as.Date('2000-01-01'),
+#'   end_date = as.Date('2000-12-31')
 #' )
 #'
 #' @rdname services_options
@@ -197,14 +219,14 @@ meteoclimatic_options <- function(
 meteogalicia_options <- function(
   resolution = c('instant', 'current_day', 'daily', 'monthly'),
   start_date = Sys.Date(),
-  end_date = NULL,
+  end_date = start_date,
   stations = NULL
 ) {
   # check arguments
   resolution <- rlang::arg_match(resolution)
   assertthat::assert_that(
     assertthat::is.date(start_date),
-    dplyr::if_else(rlang::is_null(end_date), TRUE, assertthat::is.date(end_date)),
+    assertthat::is.date(end_date),
     dplyr::if_else(rlang::is_null(stations), TRUE, rlang::is_character(stations))
   )
 
