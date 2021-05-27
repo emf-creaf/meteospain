71319273c095fba97c4531a62aa31fb1acb3e9bd
MalditoBarbudo <victorgrandagarcia@gmail.com>
2021-05-27 08:26:13 +0200

    Revert "added donttest blocks to examples requiring API Key"
    This reverts commit a90dc32eadccb1a8cbbe12c5cc73e25b617c8955.

--- R/get_data.R
+++ R/get_data.R
@@ -18,7 +18,6 @@
 #'
 #'
 #' @examples
-#' \donttest{
 #' library(meteospain)
 #' library(keyring)
 #'
@@ -31,7 +30,6 @@
 #'   api_key = key_get('aemet')
 #' )
 #' get_meteo_from('aemet', options_for_aemet)
-#' }
 #'
 #' @return An sf (spatial) object with the stations meteorological data.
 #'
@@ -66,7 +64,6 @@ get_meteo_from <- function(service = c('aemet', 'smc', 'meteoclimatic', 'meteoga
 #'   about the different services and their options.
 #'
 #' @examples
-#' \donttest{
 #' library(meteospain)
 #' library(keyring)
 #'
@@ -74,7 +71,6 @@ get_meteo_from <- function(service = c('aemet', 'smc', 'meteoclimatic', 'meteoga
 #' # key_set('aemet')
 #' api_options <- aemet_options(api_key = key_get('aemet'))
 #' get_stations_info_from('aemet', api_options)
-#' }
 #'
 #' @return An sf (spatial) object with the stations metadata.
 #'
