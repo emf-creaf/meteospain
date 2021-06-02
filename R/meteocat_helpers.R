ee0192516de8be596b50bfc0279cae5239a9388c
MalditoBarbudo <victorgrandagarcia@gmail.com>
2021-06-02 12:34:18 +0200

    added meteocat vignette

--- R/meteocat_helpers.R
+++ R/meteocat_helpers.R
@@ -493,7 +493,7 @@
 
   # Copyright message -------------------------------------------------------------------------------------
   message(
-    copyright_style("Data provided by meteo.cat \u00A9 Servei Meteorològic de Catalunya"),
+    copyright_style("Data provided by meteo.cat \u00A9 Servei Meteorol\u00F2gic de Catalunya"),
     '\n',
     legal_note_style("https://www.meteo.cat/wpweb/avis-legal/#info")
   )
