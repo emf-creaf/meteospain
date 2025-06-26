bc7f26d44bba7ff744e53f474795fd5024a057ae
MalditoBarbudo <victorgrandagarcia@gmail.com>
2025-06-26 09:50:31 +0200

    AEMET API: removed ssl config for linux so in getter

--- R/aemet_helpers.R
+++ R/aemet_helpers.R
@@ -205,11 +205,12 @@
   info_aemet <- .get_cached_result(cache_ref, {
 
     # create httr config to execute only if in linux, due to the ubuntu 20.04 update to seclevel 2
-    config_httr_aemet <- switch(
-      Sys.info()["sysname"],
-      'Linux' = httr::config(ssl_cipher_list = 'DEFAULT@SECLEVEL=1'),
-      httr::config()
-    )
+    # config_httr_aemet <- switch(
+    #   Sys.info()["sysname"],
+    #   'Linux' = httr::config(ssl_cipher_list = 'DEFAULT@SECLEVEL=1'),
+    #   httr::config()
+    # )
+    config_httr_aemet <- httr::config()
 
     # Status check ------------------------------------------------------------------------------------------
     # now we need to check the status of the response (general status), and the status of the AEMET (specific
