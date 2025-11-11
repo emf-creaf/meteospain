1c18843bd85b35888b4a72090fbe960f1cedeae2
MalditoBarbudo <victorgrandagarcia@gmail.com>
2025-11-11 15:37:33 +0100

    meteocat API: fixed urls global variable

--- R/meteocat_helpers.R
+++ R/meteocat_helpers.R
@@ -230,6 +230,7 @@
     httr2::resps_failures()
 
   if (length(failures) > 0) {
+    urls <- purrr::map(httr2::resps_requests(failures), "url")
     messages <- purrr::map_chr(
       failures,
       \(resp) {
